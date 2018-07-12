8620fc6487593e27d3f8b482c4c98f3d15ea8cef
lewmoore <lewis.moore2707@gmail.com>
2018-07-12 15:12:57 +0100

    Add netowrk interface with public ip. Add security group to ftpuat

--- AZMAGSGFTPUAT/main.tf
+++ AZMAGSGFTPUAT/main.tf
@@ -17,14 +17,33 @@ resource "azurerm_virtual_network" "vnet1" {
   name = "AZMAGVNET1"
   address_space = ["10.0.0.0/23"]
   location = "North Europe"
+}
 
-  subnet {
-    name = "AZMAGSNET11"
-    address_prefix = "10.0.0.0/24"
-  }
+resource "azurerm_subnet" "snet11" {
+  name = "AZMAGSNET11"
+  address_prefix = "10.0.0.0/24"
+  resource_group_name = "${azurerm_resource_group.vnet1rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
+}
 
-  subnet {
-    name = "AZMAGSNET12"
-    address_prefix = "10.0.1.0/24"
+resource "azurerm_public_ip" "pip1" {
+  name = "AZMAGPIPFTPUAT"
+  location = "North Europe"
+  resource_group_name = "${azurerm_resource_group.machinerg.name}"
+  public_ip_address_allocation = "static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name = "AZMAGNIC11"
+  location = "North Europe"
+  resource_group_name = "${azurerm_resource_group.machinerg.name}"
+  network_security_group_id = "${azurerm_network_security_group.ftpuat-sg.id}"
+
+  ip_configuration {
+    name = "AZMAGNIC11_ip"
+    subnet_id = "${azurerm_subnet.snet11.id}"
+    private_ip_address_allocation = "static"
+    private_ip_address = "10.0.0.4"
+    public_ip_address_id = "${azurerm_public_ip.pip1.id}"
   }
 }
---
+++ AZMAGSGFTPUAT/sg.tf
@@ -0,0 +1,29 @@
+resource "azurerm_network_security_group" "ftpuat-sg" {
+    name                = "AZMAGSGFTPUAT"
+    location            = "North Europe"
+    resource_group_name = "${azurerm_resource_group.machinerg.name}"
+
+    security_rule {
+      name                       = "AllowFTP"
+      access                     = "Allow"
+      direction                  = "Inbound"
+      priority                   = "100"
+      protocol                   = "TCP"
+      destination_port_ranges    = ["21", "5000-5050"]
+      destination_address_prefix = "*"
+      source_port_range          = "*"
+      source_address_prefix      = "217.138.46.244"
+    }
+
+    security_rule {
+      name                       = "AllowRDP"
+      access                     = "Allow"
+      direction                  = "Inbound"
+      priority                   = "110"
+      protocol                   = "TCP"
+      destination_port_range     = "3389"
+      destination_address_prefix = "*"
+      source_port_range          = "*"
+      source_address_prefix      = "217.138.46.244"
+    }
+}
