21e858d314545fc800a5102c80d8e03fdaf6bd27
lewmoore <lewis.moore2707@gmail.com>
2018-07-12 17:05:54 +0100

    Update UAT SG with correct variable. Add remote and backend to PRD and create snet resource

--- AZMAGSGFTPPRD/main.tf
+++ AZMAGSGFTPPRD/main.tf
@@ -14,3 +14,10 @@ resource "azurerm_resource_group" "ftpprdrg" {
         name = "AZMAGRGFTPPRD"
         location = "${var.location}"
 }
+
+resource "azurerm_subnet" "snet12" {
+  name = "AZMAGSNET12"
+  address_prefix = "10.0.2.0/24"
+  resource_group_name = "${data.terraform_remote_state.localstate.azurerm_resource_group_name}"
+  virtual_network_name = "${data.terraform_remote_state.localstate.virtual_network_name}"
+}
--- AZMAGSGFTPUAT/sg.tf
+++ AZMAGSGFTPUAT/sg.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_group" "ftpuat-sg" {
     name                = "AZMAGSGFTPUAT"
     location            = "${var.location}"
-    resource_group_name = "${azurerm_resource_group.machinerg.name}"
+    resource_group_name = "${azurerm_resource_group.ftpuatrg.name}"
 
     security_rule {
       name                       = "AllowFTP"
