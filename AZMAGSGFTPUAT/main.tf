0851fea302a0910b9a5f8ddeab75d8ca372c6556
lewmoore <lewis.moore2707@gmail.com>
2018-07-12 11:30:35 +0100

    Add vnet resource to create vnet1 and add to resource group

--- AZMAGSGFTPUAT/main.tf
+++ AZMAGSGFTPUAT/main.tf
@@ -11,3 +11,10 @@ resource "azurerm_resource_group" "vnetrg" {
   name = "AZMAGRGVNET1"
   location = "North Europe"
 }
+
+resource "azurerm_virtual_network" "vnet1" {
+  name = "AZMAGVNET1"
+  address_space = ["10.0.0.0/23"]
+  location = "North Europe"
+  resource_group_name = "AZMAGRGVNET1"
+}
