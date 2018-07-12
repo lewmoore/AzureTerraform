c98ae57a503107b78569ae402772ea8cb88e6d33
lewmoore <lewis.moore2707@gmail.com>
2018-07-12 11:16:58 +0100

    Add resource for creating vnet1 resource group

--- AZMAGSGFTPUAT/main.tf
+++ AZMAGSGFTPUAT/main.tf
@@ -2,7 +2,12 @@ provider "azurerm" {
 subscription_id = "${var.subscription_id}"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "machinerg" {
         name = "AZMAGRGFTPUAT"
         location = "north europe"
+}
+
+resource "azurerm_resource_group" "vnetrg" {
+  name = "AZMAGRGVNET1"
+  location = "North Europe"
 }
