6b7cbb8e44e6425c3124f1ab1bf0f87f9119af6a
lewmoore <lewis.moore2707@gmail.com>
2018-07-17 19:03:31 +0100

    Add vm resource to ftpprd and amended variables with log in details

--- AZMAGSGFTPPRD/main.tf
+++ AZMAGSGFTPPRD/main.tf
@@ -42,3 +42,33 @@ resource "azurerm_public_ip" "prdpip" {
   resource_group_name = "${azurerm_resource_group.ftpprdrg.name}"
   public_ip_address_allocation = "static"
 }
+
+resource "azurerm_virtual_machine" "FTPPRD-vm" {
+  name = "AZMAGVMCFTPPRD"
+  location = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.ftpprdrg.name}"
+  network_interface_ids = ["${azurerm_network_interface.prdnic.id}"]
+  vm_size = "Standard_DS1_v2"
+
+  storage_image_reference {
+   publisher = "MicrosoftWindowsServer"
+   offer     = "WindowsServer"
+   sku       = "2016-Datacenter"
+   version   = "latest"
+ }
+   storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+    os_profile {
+    computer_name  = "hostname"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
+
+  os_profile_windows_config {
+
+  }
+}
--- AZMAGSGFTPPRD/variables.tf
+++ AZMAGSGFTPPRD/variables.tf
@@ -1,2 +1,4 @@
 variable "subscription_id" {}
 variable "location" {}
+variable "admin_username" {}
+variable "admin_password" {}
