10ef8d4347720ee3d199044ada7f39e3a39c1bfb
lewmoore <lewis.moore2707@gmail.com>
2018-07-17 17:45:17 +0100

    Add second resource group for vnet2

--- AZMAGSGKAFUAT/main.tf
+++ AZMAGSGKAFUAT/main.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "kafuatrg" {
         name = "AZMAGRGKAFUAT"
         location = "${var.location}"
 }
+
+resource "azurerm_resource_group" "vnet2rg" {
+  name = "AZMAGRGVNET2"
+  location = "${var.location}"
+}
