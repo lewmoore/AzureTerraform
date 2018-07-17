cee347caa698b69e9575d7a2d13b6919f65faf6a
lewmoore <lewis.moore2707@gmail.com>
2018-07-17 21:48:24 +0100

    Add network interface and public ip to kafuat

--- AZMAGSGKAFUAT/main.tf
+++ AZMAGSGKAFUAT/main.tf
@@ -25,3 +25,24 @@ resource "azurerm_subnet" "snet21" {
   virtual_network_name = "${azurerm_virtual_network.vnet2.name}"
   address_prefix = "172.168.0.0/16"
 }
+
+resource "azurerm_network_interface" "kafuatnic" {
+  name = "AZMAGNIC21"
+  location = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.kafuatrg.name}"
+
+  ip_configuration {
+    name = "AZMAGNIC21-ip"
+    subnet_id = "${azurerm_subnet.snet21.id}"
+    private_ip_address_allocation = "static"
+    private_ip_address = "172.168.0.4"
+    public_ip_address_id = "${azurerm_public_ip.kafuatpip.id}"
+  }
+}
+
+resource "azurerm_public_ip" "kafuatpip" {
+  name = "AZMAGPIPKAFUAT"
+  location = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.kafuatrg.name}"
+  public_ip_address_allocation = "static"
+}
