cbd1d4b8b0f335222a466ee73fa5f9388d3c13d5
lewmoore <lewis.moore2707@gmail.com>
2018-07-17 21:34:32 +0100

    Add vnet2 to kafuat

--- AZMAGSGKAFUAT/main.tf
+++ AZMAGSGKAFUAT/main.tf
@@ -11,3 +11,10 @@ resource "azurerm_resource_group" "vnet2rg" {
   name = "AZMAGRGVNET2"
   location = "${var.location}"
 }
+
+resource "azurerm_virtual_network" "vnet2" {
+  name = "AZMAGVNET2"
+  resource_group_name = "${azurerm_resource_group.vnet2rg.name}"
+  address_space = ["172.168.0.0/16"]
+  location = "${var.location}"
+}
