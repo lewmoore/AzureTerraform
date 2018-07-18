a5d84e9c114b079a730d3bc101915b22b9de8215
lewmoore <lewis.moore2707@gmail.com>
2018-07-18 08:56:43 +0100

    Update security group with rdp

--- AZMAGSGFTPPRD/sg.tf
+++ AZMAGSGFTPPRD/sg.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_security_group" "ftpuat-sg" {
+resource "azurerm_network_security_group" "ftpprd-sg" {
     name                = "AZMAGSGFTPPRD"
     location            = "${var.location}"
     resource_group_name = "${azurerm_resource_group.ftpprdrg.name}"
@@ -9,9 +9,21 @@ resource "azurerm_network_security_group" "ftpuat-sg" {
       direction                  = "Inbound"
       priority                   = "100"
       protocol                   = "TCP"
-      destination_port_ranges    = ["22", "8000-8030"]
+      destination_port_ranges    = ["21", "5000-5050"]
       destination_address_prefix = "*"
       source_port_range          = "*"
       source_address_prefix      = "94.31.4.69"
     }
+
+      security_rule {
+        name                       = "AllowRDP"
+        access                     = "Allow"
+        direction                  = "Inbound"
+        priority                   = "110"
+        protocol                   = "TCP"
+        destination_port_range     = "3389"
+        destination_address_prefix = "*"
+        source_port_range          = "*"
+        source_address_prefix      = "94.31.4.69"
+      }
 }
